092167f5b78b597b3f6cd696a35bea170b8fdbf3
Kier <reyeskrct@gmail.com>
2025-03-12 22:27:09 +0800

    modify-build-dependencies

--- modules/cloudsim-examples/src/main/java/CloudSimTestRR/SimulationController.java
+++ modules/cloudsim-examples/src/main/java/CloudSimTestRR/SimulationController.java
@@ -1,12 +1,17 @@
 package CloudSimTestRR;
 
-import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173") // Allow React frontend
+@CrossOrigin(origins = "*")// Allow React frontend
 public class SimulationController {
 
     private String simulationResults = "{}";
